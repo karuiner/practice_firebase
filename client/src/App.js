315ff78ef4f286dfee31acfd51c34defd5379f63
karuiner <intaekgong@gmail.com>
2022-09-27 22:01:17 +0900

    구글 이메일로의 로그인 따라하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import Header from "./components/Header";
-
+import finit from "./FirebaseInit";
+import { getAuth } from "firebase/auth";
+import { useState } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 const Frame = styled.div`
   height: 100vh;
   width: 100vw;
@@ -12,14 +15,31 @@ const Main = styled.div`
   height: 100%;
   width: ${() => (window.innerWidth > 1200 ? "1200px" : "100%")};
   display: flex;
+  flex-direction: column;
   border: solid 1px red;
 `;
+const auth = getAuth(finit);
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  function handleGoogleLogin() {
+    const provider = new GoogleAuthProvider(); // provider를 구글로 설정
+    signInWithPopup(auth, provider) // popup을 이용한 signup
+      .then((data) => {
+        setUserData(data.user); // user data 설정
+        console.log(data); // console로 들어온 데이터 표시
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
     <Frame>
       <Main>
         <Header></Header>
+        <button onClick={handleGoogleLogin}>Login</button>
+        {userData ? userData.displayName : null}
       </Main>
     </Frame>
   );
